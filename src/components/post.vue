8fa4d91d2ed7143b7e9b161403c05d6aeace562c
Xeltica <citringo@gmail.com>
2018-10-01 00:43:40 +0900

    とりあえずホイ

---
+++ src/components/post.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+	article.post
+		header.stack
+			h3.name {{ name }}
+			aside.badge {{  }}
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'vue-property-decorator';
+@Component({})
+export default class Post extends Vue
+{
+	@Prop() name = "";
+	@Prop() isAdmin = false;
+	@Prop() reactions: ReactionViewModel[] = [];
+	@Prop() isStarred = false;
+	@Prop() time = "";
+}
+
+interface ReactionViewModel
+{
+	reactionChar: string;
+	reactionCount: number;
+	isMyReaction: boolean;
+}
+
+</script>
+
+
+<style lang="sass" scoped>
+
+</style>
