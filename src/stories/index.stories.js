56a05e2dcc8161bbedfcb474b3821e3277373e0f
Xeltica <citringo@gmail.com>
2018-09-30 20:53:19 +0900

    update storybook

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,17 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from '@storybook/vue'
-import { action } from '@storybook/addon-actions'
+import { storiesOf } from "@storybook/vue"
+import { action } from "@storybook/addon-actions"
 
-import MyButton from '../components/MyButton.vue'
+import MyButton from "../components/MyButton.vue"
+import HelloWorld from "../components/HelloWorld.vue"
 
-storiesOf('Button', module)
-	.add('with text', () => ({
-		components: { MyButton },
-		template: '<my-button @click="action">Hello Button</my-button>',
-		methods: { action: action('clicked') }
+storiesOf("Hello World", module)
+	.add("hello world", () => ({
+		components: { HelloWorld },
+		template: "<hello-world msg='Welcome to Your Vue.js + TypeScript App'/>",
 	}))
-	.add('with some emoji', () => ({
+
+storiesOf("Button", module)
+	.add("with text", () => ({
 		components: { MyButton },
-		template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-		methods: { action: action('clicked') }
+		template: "<my-button @click='action'>Hello Button</my-button>",
+		methods: { action: action("clicked") }
 	}))
+	.add("with some emoji", () => ({
+		components: { MyButton },
+		template: "<my-button @click='action'>😀 😎 👍 💯</my-button>",
+		methods: { action: action("clicked") }
+	}));
