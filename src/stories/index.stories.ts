223449149b571ae4d8c17a9cc49111abca17b248
Xeltica <citringo@gmail.com>
2018-10-09 09:24:08 +0900

    refactor

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -66,9 +66,10 @@ storiesOf("Header", module)
 storiesOf("Window", module)
 	.add("window", () => ({
 		components: { Window },
-		template: `<window title="New Window" width="400px" height="400px">
+		template: `
+		<window title="New Window" width="400px" height="400px">
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</window>`
+		</window>`,
 	}))
 	.add("create new post window", () => ({
 		components: { CreateNewPost },
